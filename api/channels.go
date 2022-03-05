7623494978a889743ba4b2ad9698447ec9d700b6
goodwitchh <80130573+goodwitchh@users.noreply.github.com>
2022-03-04 20:43:16 -0500

    Update channels.go

--- api/channels.go
+++ api/channels.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"sync"
 
-	"github.com/Not-Cyrus/GoRaider/utils"
+	"https://github.com/goodwitchh/GoRaider/tree/main/utils"
 )
 
 func deleteChannel(channelID string, wg *sync.WaitGroup) {
